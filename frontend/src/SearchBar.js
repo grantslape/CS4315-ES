4b61a866f9a6b14005dae65b018a0da7783ad00d
Grant Slape <me@grantslape.com>
2019-02-21 18:04:16 -0600

    fix gitignore, fix indentation

--- frontend/src/SearchBar.js
+++ frontend/src/SearchBar.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    render() {
-        return (
-            <form>
-                <label>
-                    <input
-                        type="text"
-                        autoFocus
-                        placeholder="Enter query" 
-                    />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form>
+         <label>
+            <input type="text" autoFocus placeholder="Enter query" />
+          </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 export default SearchBar;
